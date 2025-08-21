dd19c31dc7e213b13bc0f6d350a16614e47838c9
hudsonsSMS <hudsons@stmartins.school.nz>
2025-08-21 20:37:18 +1200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ from scratchclient import ScratchSession
 import time
 import requests
 import json
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Online!"
+
+if __name__ == "__main__":
+    port = 1000  # default 10000 if PORT not set
+    app.run(host="0.0.0.0", port=port)
+
 
 airesponse = ""
 
